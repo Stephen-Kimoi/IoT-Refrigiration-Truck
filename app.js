e6ad7e3e90af856bc1540bf853370b2846dc38e2
steve kimoi <87986813+Stephen-Kimoi@users.noreply.github.com>
2021-11-22 14:35:09 +0300

    adding the functions to get routes using Azure Maps

--- app.js
+++ app.js
@@ -103,4 +103,104 @@ var truckOnSection; // The current path section the truck is on.
 var truckSectionsCompletedTime; // The time the truck has spent on previous completed sections.
 
 
+function Degrees2Radians(deg) {
+    return deg * Math.PI / 180;
+}
+
+function DistanceInMeters(lat1, lon1, lat2, lon2) {
+    var dlon = Degrees2Radians(lon2 - lon1);
+    var dlat = Degrees2Radians(lat2 - lat1);
+    var a = (Math.sin(dlat / 2) * Math.sin(dlat / 2)) + Math.cos(Degrees2Radians(lat1)) * Math.cos(Degrees2Radians(lat2)) * (Math.sin(dlon / 2) * Math.sin(dlon / 2));
+    var angle = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var meters = angle * 6371000;
+    return meters;
+}
+
+function Arrived() {
+
+    // If the truck is within 10 meters of the destination, call it good. 
+    if (DistanceInMeters(currentLat, currentLon, destinationLat, destinationLon) < 10)
+        return true;
+    return false;
+}
+
+function UpdatePosition() {
+    while ((truckSectionsCompletedTime + timeOnPath[truckOnSection] < timeOnCurrentTask) && (truckOnSection < timeOnPath.length - 1)) {
+        // Truck has moved on to the next section. 
+        truckSectionsCompletedTime += timeOnPath[truckOnSection];
+        ++truckOnSection;
+    }
+
+    // Ensure remainder is less than or equal to 1, because the interval may take count over what is needed. 
+    var remainderFraction = Math.min(1, (timeOnCurrentTask - truckSectionsCompletedTime) / timeOnPath[truckOnSection]);
+
+    // The path should be one entry longer than the timeOnPath array. 
+    // Find how far along the section the truck has moved. 
+    currentLat = path[truckOnSection][0] + remainderFraction * (path[truckOnSection + 1][0] - path[truckOnSection][0]);
+    currentLon = path[truckOnSection][1] + remainderFraction * (path[truckOnSection + 1][1] - path[truckOnSection][1]);
+}
+
+function GetRoute(newState) {
+
+    // Set the state to ready, until the new route arrives. 
+    state = stateEnum.ready;
+
+    // Coordinates are in longitude first. 
+    var coordinates = [
+        [currentLon, currentLat],
+        [destinationLon, destinationLat]
+    ];
+    var results = routeURL.calculateRouteDirections(rest.Aborter.timeout(10000), coordinates);
+    results.then(data => {
+        greenMessage("Route found. Number of points = " + JSON.stringify(data.routes[0].legs[0].points.length, null, 4));
+
+        // Clear the path. 
+        path.length = 0;
+
+        // Start with the current location. 
+        path.push([currentLat, currentLon]);
+
+        // Retrieve the route and push the points onto the array. 
+        for (var n = 0; n < data.routes[0].legs[0].points.length; n++) {
+            var x = data.routes[0].legs[0].points[n].latitude;
+            var y = data.routes[0].legs[0].points[n].longitude;
+            path.push([x, y]);
+        }
+
+        // Finish with the destination. 
+        path.push([destinationLat, destinationLon]);
+
+        // Store the path length and the time taken to calculate the average speed. 
+        var meters = data.routes[0].summary.lengthInMeters;
+        var seconds = data.routes[0].summary.travelTimeInSeconds;
+        var pathSpeed = meters / seconds;
+        var distanceApartInMeters;
+        var timeForOneSection;
+
+        // Clear the time on the path array. 
+        timeOnPath.length = 0;
+
+        // Calculate how much time is required for each section of the path. 
+        for (var t = 0; t < path.length - 1; t++) {
+
+            // Calculate the distance between the two path points, in meters. 
+            distanceApartInMeters = DistanceInMeters(path[t][0], path[t][1], path[t + 1][0], path[t + 1][1]);
+
+            // Calculate the time for each section of the path. 
+            timeForOneSection = distanceApartInMeters / pathSpeed;
+            timeOnPath.push(timeForOneSection);
+        }
+        truckOnSection = 0;
+        truckSectionsCompletedTime = 0;
+        timeOnCurrentTask = 0;
+
+        // Update the state now that the route has arrived, either enroute or returning. 
+        state = newState;
+    }, reason => {
+
+        // Error: The request was aborted. 
+        redMessage(reason);
+        eventText = "Failed to find map route";
+    });
+}
 
